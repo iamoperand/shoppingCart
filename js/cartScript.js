c87baffe55411e7eaa8aa783d98575b549bab4f4
Nikhil Arora <narora200@gmail.com>
2017-06-10 05:16:38 +0530

    Add, sub, del working

--- js/cartScript.js
+++ js/cartScript.js
@@ -14,14 +14,84 @@ $(function(){
 	}else{
 		var newList = '';
 		for(var i=0; i<cartList.length;i++){
-			 newList = $('<tr><td>' + cartList[i].name + '</td><td>' +
-			cartList[i].price + '</td><td>' + cartList[i].size + '</td><td>' + cartList[i].quantity + 
-			'</td><td>' + cartList[i].quantity * cartList[i].price + '</td></tr>');
+
+			if(cartList[i].quantity > 0){
+				console.log('newlist created');
+				newList = $('<tr data-row='+i+'><td>' + cartList[i].name + '</td><td>' +
+			cartList[i].price + '</td><td>' + cartList[i].size + '</td><td data-quantity='+i+'>' + cartList[i].quantity + 
+			'</td><td data-price='+i+'>' + cartList[i].quantity * cartList[i].price + '</td><td><button data-id='+i+
+			' class="btn btn-primary button-add">Add</button></td><td><button data-id='+i+' class="btn btn-primary button-sub">Sub</button></td>'+
+			'<td><button data-id='+i+' class="btn btn-primary button-del">Del</button></td></tr>');
+			 console.log(newList);
 
 			newList.prependTo('tbody');
+			}
+			 
 	}
+
+
+	$('.button-add').click(function(){
+		console.log("this object (Array element)", $(this)[0]);
+		console.log("this object (Array)", $(this));
+		var parenting = $(this).parent();
+
+		var parent = $(this)[0].parentElement;
+		var ancestor = parent.parentNode;
+		var quantityElement = ancestor.children[3];
+		console.log("quantityElement: ", quantityElement);
+		console.log(cartList);
+		console.log(cartList[parseInt($(this).attr('data-id'))].quantity)
+		cartList[parseInt($(this).attr('data-id'))].quantity++;
+		console.log(parseInt($(this).attr('data-id')));
+		saveProducts();
+		
+        refreshProducts();
+                
+			
+	});
+	$('.button-sub').click(function(){
+		console.log("this object (Array element)", $(this)[0]);
+		console.log("this object (Array)", $(this));
+		var parenting = $(this).parent();
+
+		var parent = $(this)[0].parentElement;
+		var ancestor = parent.parentNode;
+		var quantityElement = ancestor.children[3];
+		console.log("quantityElement: ", quantityElement);
+		
+		cartList[parseInt($(this).attr('data-id'))].quantity--;
+		if(cartList[parseInt($(this).attr('data-id'))].quantity <= 0){
+			ancestor.remove();
+		}
+		
+		console.log(parseInt($(this).attr('data-id')));
+		saveProducts();
+		
+        refreshProducts();	
+
+		//quantityElement.innerText--;
+			
+	});
+	$('.button-del').click(function(){
+		console.log("this object (Array element)", $(this)[0]);
+		console.log("this object (Array)", $(this));
+		cartList[parseInt($(this).attr('data-id'))].quantity = 0;
+		var parenting = $(this).parent();
+
+		var parent = $(this)[0].parentElement;
+		var ancestor = parent.parentNode;
+		ancestor.remove();
+		saveProducts();
+		
+        refreshProducts();	
+
+		//var quantityElement = ancestor.children[3];
+		//console.log("quantityElement: ", quantityElement);
+		
+			
+	});
 }
-	var tr = document.createElement('tr');
+	//var tr = document.createElement('tr');
 
 
 
--- js/mainScript.js
+++ js/mainScript.js
@@ -1,6 +1,12 @@
 var cartList = [];
  function saveProducts () {
-            localStorage.setItem('cartList', JSON.stringify(cartList));
+            for(var product of cartList){
+            	console.log('checking');
+            	console.log("product ", product);
+            	
+            	localStorage.setItem('cartList', JSON.stringify(cartList));
+            
+            }
             console.log('Getting saved into local storage');
         }
         function retrieveProducts() {
@@ -8,3 +14,36 @@ var cartList = [];
             cartList = JSON.parse(
                 localStorage.getItem('cartList') ? localStorage.getItem('cartList') : '[]');
         }
+
+         function refreshProducts () {
+            
+            //Getting refreshed
+            //retrieveProducts();
+            console.log('View getting refreshed');
+            cartList = JSON.parse(
+                localStorage.getItem('cartList') ? localStorage.getItem('cartList') : '[]');
+
+            var newList = '';
+		console.log('Inside refresh products: ', cartList.length);
+		
+		for(var i=0; i<cartList.length;i++){
+			 //console.log("checking inner text ", parseInt($('[data-quantity='+i+']')[0].innerText));
+			
+
+			//if(parseInt($('[data-quantity='+i+']')[0].innerText) <= 0){
+			//	console.log('quantity less than or equal to zero now. Changing view');
+			//	console.log($('[data-row='+i+']')[0]);
+				
+			//}else{
+				console.log(cartList[i].quantity);
+				if(cartList[i].quantity > 0){
+					console.log($('[data-quantity='+i+']')[0].innerText);
+			$('[data-quantity='+i+']')[0].innerText = cartList[i].quantity;
+			$('[data-price='+i+']')[0].innerText = cartList[i].quantity * cartList[i].price;
+        
+				}
+				
+			//}
+			
+        }
+    }
