92315ded88276da49954bf230c8abc4d76628fea
Nikhil Arora <narora200@gmail.com>
2017-06-10 06:30:43 +0530

    indexScript.js working fine

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,5 +1,5 @@
 
-
+//Constructor function for creating object
 function Items(name, price, size, quantity){
     this.name = name;
     this.price = price;
@@ -9,34 +9,44 @@ function Items(name, price, size, quantity){
 
        
 $(function(){
+    //retrieveProducts() is called at the start to fill cartList array with string from localStorage 
     retrieveProducts();
+
+    //9 is the number of products in the Shopping Web-App
     for(var i=1; i<=9;i++){
 
+        //Click event handlers for AddToCart buttons identified by ['data-id=<id>']
         $('[data-id = ' + i + ']').click(function () {
-            // body...
-            var obj = $(this);
-            console.log("obj is ", obj);
-            var siblings = obj.siblings();
-            var name;
-            var price;
-            var size;
-            var quantity;
-            console.log("$ object " , $('[data-id = ' + i + ']'));
-            console.log("this object " , siblings);
+           
+            //var obj = $(this);
+            //console.log("obj is ", obj);
+
+            //Get the siblings of the button-element (AddToCart Button)
+            var siblings = $(this).siblings();
+            var name, price, size, quantity;
+            
             price = siblings[0].innerText;
             size = siblings[2].innerText;
             quantity = siblings[4].value;
+            
+            //Accessing the name of the product which is the sibling of the parent element
             var parent = siblings.parent();
-            console.log('Parent ' ,  parent);
+            //console.log('Parent ' ,  parent);
+            
+
             name = parent.siblings()[0].innerText;
             // console.log("ith value " + i);
 
-            console.log('Price: ' + price);
-            console.log('Size: ' + size);
-            console.log('Quantity: ' + quantity);
-            console.log('Name: ' + name);
+            //console.log('Price: ' + price);
+            //console.log('Size: ' + size);
+            //console.log('Quantity: ' + quantity);
+            //console.log('Name: ' + name);
 
+            
+            //For-loop for checking if the cartList array contains the product initially
             for(var j=0; j<cartList.length; j++){
+                
+                //Flag to check the same product exists in the array or not
                 var flag = 0;
                 console.log('product is: ', cartList[j]);
                 console.log(cartList[j].name);
@@ -48,21 +58,20 @@ $(function(){
                     flag = 1;
                 }
                     
-                }
+            }
             
+            //If not found, then create a new object and push it into the array
             if(!flag){
                 var newItem = new Items(name, price, size, quantity);
-            console.log("This is new item: " , newItem);
-
-
-            cartList.push(newItem);
-            
+                console.log("This is new item: " , newItem);
+                cartList.push(newItem);
             }
+            
             console.log("cartList Array is: ", cartList);
 
-        
+            //Push the changes to localStorage as well
             saveProducts();
-            });
+        });
     }
 
 
--- js/mainScript.js
+++ js/mainScript.js
@@ -1,4 +1,9 @@
 var cartList = [];
+ function retrieveProducts() {
+        	console.log('Getting retrieved');
+            cartList = JSON.parse(
+                localStorage.getItem('cartList') ? localStorage.getItem('cartList') : '[]');
+        }
  function saveProducts () {
             for(var product of cartList){
             	console.log('checking');
@@ -9,41 +14,37 @@ var cartList = [];
             }
             console.log('Getting saved into local storage');
         }
-        function retrieveProducts() {
-        	console.log('Getting retrieved');
-            cartList = JSON.parse(
-                localStorage.getItem('cartList') ? localStorage.getItem('cartList') : '[]');
-        }
-
-         function refreshProducts () {
-            
-            //Getting refreshed
-            //retrieveProducts();
-            console.log('View getting refreshed');
-            cartList = JSON.parse(
-                localStorage.getItem('cartList') ? localStorage.getItem('cartList') : '[]');
-
-            var newList = '';
-		console.log('Inside refresh products: ', cartList.length);
-		
-		for(var i=0; i<cartList.length;i++){
-			 //console.log("checking inner text ", parseInt($('[data-quantity='+i+']')[0].innerText));
-			
-
-			//if(parseInt($('[data-quantity='+i+']')[0].innerText) <= 0){
-			//	console.log('quantity less than or equal to zero now. Changing view');
-			//	console.log($('[data-row='+i+']')[0]);
-				
-			//}else{
-				console.log(cartList[i].quantity);
-				if(cartList[i].quantity > 0){
-					console.log($('[data-quantity='+i+']')[0].innerText);
-			$('[data-quantity='+i+']')[0].innerText = cartList[i].quantity;
-			$('[data-price='+i+']')[0].innerText = cartList[i].quantity * cartList[i].price;
         
-				}
-				
-			//}
-			
-        }
-    }
+
+function refreshProducts () {
+
+//Getting refreshed
+//retrieveProducts();
+console.log('View getting refreshed');
+cartList = JSON.parse(
+    localStorage.getItem('cartList') ? localStorage.getItem('cartList') : '[]');
+
+var newList = '';
+console.log('Inside refresh products: ', cartList.length);
+
+for(var i=0; i<cartList.length;i++){
+ //console.log("checking inner text ", parseInt($('[data-quantity='+i+']')[0].innerText));
+
+
+//if(parseInt($('[data-quantity='+i+']')[0].innerText) <= 0){
+//	console.log('quantity less than or equal to zero now. Changing view');
+//	console.log($('[data-row='+i+']')[0]);
+	
+//}else{
+	console.log(cartList[i].quantity);
+	if(cartList[i].quantity > 0){
+		console.log($('[data-quantity='+i+']')[0].innerText);
+$('[data-quantity='+i+']')[0].innerText = cartList[i].quantity;
+$('[data-price='+i+']')[0].innerText = cartList[i].quantity * cartList[i].price;
+
+	}
+	
+//}
+
+}
+}
